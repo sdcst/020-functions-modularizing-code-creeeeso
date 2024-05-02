3a5c14b516567df95f9f949bc7d0232acb0e8b25
creeeeso <crystalf7319@gmail.com>
2024-05-02 12:40:28 -0700

    1

--- task2.py
+++ task2.py
@@ -8,11 +8,29 @@ displays instructions and how to play
 game()
 plays the games
 """
+
 def title():
     print("Try guess the number in 10 turns")
-x=int(input("Choose a random number between 1 and 100: "))
-import random
-y=random.randient(1,100)
-while x==y:
-    print
-
+title()
+def game():
+    x=int(input("Choose a random number between 1 and 100: "))
+    import random
+    y=random.randint(1,100)
+    z=0
+    while x!=y:
+        if x>y:
+            print("The number is over")
+            z+=1
+            x=int(input("Choose a random number between 1 and 100: "))
+        else:
+            print('The number is under')
+            z+=1
+            x=int(input("Choose a random number between 1 and 100: "))
+        if z == 10: 
+            print("You have tried 10 times now! :(")
+            x=int(input("Choose a random number between 1 and 100: "))
+    if x==y:
+        print("You got it!")
+        z+=1
+        print(f"You have tried {z} times in total")
+game()
